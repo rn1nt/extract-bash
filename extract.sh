95f21b7b939333a71297e04b678dd2a3bfdcc56e
rn1nt <53822480+rn1nt@users.noreply.github.com>
2022-05-04 18:13:15 +0800

    Add find command

--- extract.sh
+++ extract.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
 touch 2022-02-28_input.txt
-cat ./daily.log.2022-02-28 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-02-28_input.txt
+cat $(find . -name daily.log.2022-02-28) | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-02-28_input.txt
 
 touch 2022-03-01_input.txt
-cat ./daily.log.2022-03-01 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-01_input.txt
+cat $(find . -name daily.log.2022-03-01) | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-01_input.txt
 
 touch 2022-03-02_input.txt
-cat ./daily.log.2022-03-02 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-02_input.txt
+cat $(find . -name daily.log.2022-03-02) | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-02_input.txt
 
 touch 2022-03-03_input.txt
-cat ./daily.log.2022-03-03 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-03_input.txt
+cat $(find . -name daily.log.2022-03-03) | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-03_input.txt
 
 touch 2022-03-04_input.txt
-cat ./daily.log.2022-03-04 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-04_input.txt
+cat $(find . -name daily.log.2022-03-04) | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-04_input.txt
 
 touch 2022-03-05_input.txt
-cat ./daily.log.2022-03-05 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-05_input.txt
+cat $(find . -name daily.log.2022-03-05) | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-05_input.txt
 
  
