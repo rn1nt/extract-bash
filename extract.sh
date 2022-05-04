7f3825ea23ce733c6a6745ed93a2bc59e4af73c8
rn1nt <53822480+rn1nt@users.noreply.github.com>
2022-05-04 15:17:31 +0800

    Add files via upload

---
+++ extract.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+touch 2022-02-28_input.txt
+cat ./daily.log.2022-02-28 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-02-28_input.txt
+cat ./2022-02-28_input.txt | sed 's/\(.*\)\(hello\)\(.*\)/\3\/\1\/\2/' > 2022-02-28_inout.txt
+
+
+
+
+touch 2022-03-01_input.txt
+cat ./daily.log.2022-03-01 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-01_input.txt
+
+touch 2022-03-02_input.txt
+cat ./daily.log.2022-03-02 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-02_input.txt
+
+touch 2022-03-03_input.txt
+cat ./daily.log.2022-03-03 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-03_input.txt
+
+touch 2022-03-04_input.txt
+cat ./daily.log.2022-03-04 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-04_input.txt
+
+touch 2022-03-05_input.txt
+cat ./daily.log.2022-03-05 | grep -aE "\[.*?]\[INFO\]\[传入参数为.*?\]" > 2022-03-05_input.txt
+
+ 
